f14ddb8d05479ab086ee14971a9667db3a02b8d7
zhufengzhufeng <894918097@qq.com>
2018-01-23 10:56:48 +0800

    '3.导航配置'

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Tab from "../components/Tab/Tab";
 export default class App extends React.Component {
   render(){
     return <div>
       {this.props.children}
+      <Tab/>
     </div>
   }
 }
