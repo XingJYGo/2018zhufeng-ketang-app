93bc5b80523be62ff21bbf2aed3cd240707cb7f1
zhufengzhufeng <894918097@qq.com>
2018-01-23 12:31:18 +0800

    '6.transiton'

--- src/containers/Home/HomeHeader.js
+++ src/containers/Home/HomeHeader.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import logo from '../../common/images/logo.png'
+import logo from '../../common/images/logo.png';
+// 引入transition组件
+import Transition from 'react-transition-group/Transition';
+// 默认样式
+const duration = 300;//动画时间
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+const transitionStyles = { //到时候可能状态是entering或者 entered
+  entering: { opacity: 0 },
+  entered:  { opacity: 1 },
+};
 export default class HomeHeader extends React.Component {
   constructor(){
     super();
@@ -18,11 +30,18 @@ export default class HomeHeader extends React.Component {
               <i className="iconfont icon-guanbi"></i>:<i className="iconfont icon-liebiao"></i>}
           </div>
         </div>
-        <ul className="header-menu">
-          <li>全部课程</li>
-          <li>React课程</li>
-          <li>Vue课程</li>
-        </ul>
+        <Transition in={this.state.isShow} timeout={duration}>
+          {(state)=>(
+            <ul className="header-menu" style={{
+              ...defaultStyle,
+              ...transitionStyles[state]
+            }}>
+              <li>全部课程</li>
+              <li>React课程</li>
+              <li>Vue课程</li>
+            </ul>
+          )}
+        </Transition>
       </div>)
   }
 }
