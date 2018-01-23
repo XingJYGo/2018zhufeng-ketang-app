18cf6674a50a652404817c891b7a044a440bef21
zhufengzhufeng <894918097@qq.com>
2018-01-23 10:39:12 +0800

    '2.路由配置'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import React,{Component} from 'react'
 import ReactDOM,{render} from 'react-dom';
-ReactDOM.render('hello world',window.root);
+import {HashRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
+// 配置路由的组件
+import Home from "./containers/Home/Home";
+import Profile from "./containers/Profile/Profile";
+import Lesson from "./containers/Lesson/Lesson";
+import App from "./containers/App";
+// 组件分为容器组件和基础组件
+ReactDOM.render(<Router>
+  <App>
+    <Switch> {/*匹配path 匹配后渲染 并且不再继续渲染*/}
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/lesson" component={Lesson}/>
+      <Route path="/profile" component={Profile}/>
+    </Switch>
+  </App>
+</Router>,window.root);
