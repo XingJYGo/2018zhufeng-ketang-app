46b077183a1e6139c66b9cd084fbf7ee18021dbe
zhufengzhufeng <894918097@qq.com>
2018-01-23 15:42:50 +0800

    '9.redux流程'

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './index.less'
 import HomeHeader from "./HomeHeader";
+import {connect} from 'react-redux';
+import actions from '../../store/actions/home'
+@connect(state=>({...state.home}),actions)
 export default class Home extends React.Component {
   // 选择当前哪门课程，做筛选用
   selectCurrentLesson = (val) =>{ // val是当前选择的课程
-    alert(val);
+      this.props.updateCurrentLesson(val);
   };
   render(){
     return <div>
--- src/store/actions/home.js
+++ src/store/actions/home.js
@@ -1,3 +1,8 @@
-/**
- * Created by speedly on 2018/1/23.
- */
+import * as Types from '../action-types';
+let actions = {
+  // 更新当前选择的课程
+  updateCurrentLesson(lesson){
+    return {type:Types.SET_CURRENT_LESSON,lesson}
+  }
+};
+export default actions;
