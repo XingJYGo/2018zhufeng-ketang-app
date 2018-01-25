0b03e7570dc1a920ccb055d01a5fecd05823d607
zhufengzhufeng <894918097@qq.com>
2018-01-25 15:34:01 +0800

    '2.counter'

--- src/Counter.js
+++ src/Counter.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import store from './store/index'
+import store from './store/index';
+import actions from './store/action'
 export default class Counter extends React.Component {
+  constructor(){
+    super();
+    this.state = {number:store.getState().number}
+  }
+  componentDidMount(){
+    store.subscribe(()=>{
+      this.setState({number:store.getState().number})
+    });
+  }
   render(){
-    return <div>{store.getState().number}</div>
+    return <div>
+      <button onClick={()=>{
+        store.dispatch(actions.add())
+      }}>+</button>
+      {this.state.number}
+      <button onClick={()=>{
+        store.dispatch(actions.minus())
+      }}>-</button>
+      </div>
   }
 }
