235390710cbf6814762d9f2663d11b607dad467f
zhufengzhufeng <894918097@qq.com>
2018-01-25 12:49:14 +0800

    '27.代码分割'

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import React,{Component} from 'react'
 import ReactDOM,{render} from 'react-dom';
 import {HashRouter as Router,Route,Switch,Redirect} from 'react-router-dom'
 // 配置路由的组件
-import Home from "./containers/Home/Home";
-import Profile from "./containers/Profile/Profile";
-import Lesson from "./containers/Lesson/Lesson";
+
 import App from "./containers/App";
 import Detail from "./containers/Detail/Detail";
 // 组件分为容器组件和基础组件
@@ -13,6 +11,11 @@ import store from './store'
 import Login from "./containers/Login/Login";
 import Reg from "./containers/Reg/Reg";
 import ProtectedRoute from "./ProtectedRoute";
+
+import syncComponent from './SyncComponent'
+let Home = syncComponent(()=>import("./containers/Home/Home"));
+let Lesson = syncComponent(()=>import("./containers/Lesson/Lesson"));
+let Profile = syncComponent(()=>import("./containers/Profile/Profile"));
 ReactDOM.render(
   <Provider store={store}>
     <Router>
